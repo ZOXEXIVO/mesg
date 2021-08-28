ece0519aa07fc07555972f3925f00dfa5a9d9592
ZOXEXIVO <zoxexivo@gmail.com>
2021-08-28 19:35:35 +0300

    Fix

--- src/server/network/response.rs
+++ src/server/network/response.rs
@@ -3,6 +3,7 @@ use crate::storage::StorageReader;
 use log::info;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use tokio::time::{sleep, Duration};
 use tonic::codegen::futures_core::Stream;
 
 pub struct PullResponseStream {
@@ -13,15 +14,22 @@ impl Stream for PullResponseStream {
     type Item = std::result::Result<PullResponse, tonic::Status>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        info!("poll_next");
+
         match self.reader.storage.get_mut(&self.reader.queue_name) {
             Some(mut store) => {
+                info!("store exists");
                 if let Some(item) = store.data.pop_front() {
+                    info!("return item");
+
                     Poll::Ready(Some(Ok(PullResponse {
                         len: item.data.len() as i32,
                         data: item.data.to_vec(),
                         message_id: item.id.to_string(),
                     })))
                 } else {
+                    info!("item not found. subscribe");
+
                     let waker = cx.waker().clone();
                     let mut reciever = store.notification.subscribe();
 
@@ -34,7 +42,16 @@ impl Stream for PullResponseStream {
                     Poll::Pending
                 }
             }
-            None => Poll::Pending,
+            None => {
+                let waker = cx.waker().clone();
+
+                tokio::spawn(async move {
+                    sleep(Duration::from_millis(1000)).await;
+                    waker.wake();
+                });
+
+                Poll::Pending
+            }
         }
     }
 }
