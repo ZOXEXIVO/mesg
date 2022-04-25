3067488b7910f111024e5c323d0a372b8afa8eec
ZOXEXIVO <zoxexivo@gmail.com>
2022-04-26 02:45:45 +0300

    storage refactoring

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,10 +1,8 @@
 mod identity;
 mod message;
-mod queues;
 mod storage;
 mod utils;
 
 pub use message::*;
-pub use queues::*;
 pub use storage::*;
 pub use utils::*;
--- src/storage/storage.rs
+++ src/storage/storage.rs
@@ -1,19 +1,16 @@
 use crate::storage::identity::Identity;
 use crate::storage::message::Message;
-use crate::storage::{NameUtils, QueueCollection};
+use crate::storage::{NameUtils, QueueNames};
 use bytes::Bytes;
 use log::{error, info};
 use sled::{Db, IVec, Subscriber};
 use std::collections::HashMap;
 use std::sync::Arc;
-use structopt::clap::App;
 use thiserror::Error;
 use tokio::sync::RwLock;
 
-type DbInternal = (Db, QueueCollection);
-
 pub struct Storage {
-    store: Arc<RwLock<HashMap<String, DbInternal>>>,
+    store: Arc<RwLock<HashMap<String, Db>>>,
 }
 
 impl Storage {
@@ -25,51 +22,36 @@ impl Storage {
 
     pub async fn subscribe(&self, queue: &str, application: &str) -> Option<Subscriber> {
         let result = self
-            .execute_in_context(queue, move |db, queues| {
+            .execute_in_context(queue, move |db| {
                 let queue_names = NameUtils::application(application);
 
-                queues.execute(db, queue_names.default(), |tree| {
-                    Some(tree.watch_prefix(vec![]))
-                })
+                db.open_tree(queue_names.default())
+                    .unwrap()
+                    .watch_prefix(vec![])
             })
             .await;
 
         match result {
-            Ok(res) => res,
+            Ok(res) => Some(res),
             Err(_) => None,
         }
     }
 
     pub async fn push(&self, queue: &str, data: Bytes) -> Result<bool, StorageError> {
         let result = self
-            .execute_in_context(queue, move |db, queues| {
+            .execute_in_context(queue, move |db| {
                 let (identity_val, identity_vec) = Identity::get(db, queue);
                 let message = Message::new(identity_val, Bytes::clone(&data));
 
                 let mut pushed = false;
 
-                for tree_name in queues.tree_names() {
-                    queues.execute(db, &tree_name, |app_queue| {
-                        app_queue
-                            .insert(&identity_vec, message.clone().data.to_vec())
-                            .unwrap();
-                        //
-                        // db.flush();
-                        //
-                        // let available_items = app_queue
-                        //     .iter()
-                        //     .keys()
-                        //     .map(|k| {
-                        //         u64::from_be_bytes(k.unwrap().to_vec().try_into().unwrap())
-                        //             .to_string()
-                        //     })
-                        //     .fold(String::new(), |a, b| a + ", " + &b);
-                        //
-                        // info!(
-                        //     "message pushed, message_id={}, available_items={}",
-                        //     message.id, available_items
-                        // );
-                    });
+                for queue_name in Self::queue_names(db) {
+                    let queue_names = NameUtils::application(&queue_name);
+
+                    db.open_tree(queue_names.default())
+                        .unwrap()
+                        .insert(&identity_vec, message.clone().data.to_vec())
+                        .unwrap();
 
                     pushed = true;
                 }
@@ -86,25 +68,24 @@ impl Storage {
 
     pub async fn pop(&self, queue: &str, application: &str) -> Option<Message> {
         let result = self
-            .execute_in_context(queue, move |db, queues| {
+            .execute_in_context(queue, move |db| {
                 let queue_names = NameUtils::application(application);
 
-                return queues.execute(db, queue_names.default(), |original_queue| {
-                    if let Ok(Some((k, v))) = original_queue.pop_min() {
-                        queues.execute(db, queue_names.unacked(), |unacked_queue| {
-                            unacked_queue.insert(k.clone(), v.clone()).unwrap();
-                        });
+                let original_queue = db.open_tree(queue_names.default()).unwrap();
+                let unacked_queue = db.open_tree(queue_names.unacked()).unwrap();
 
-                        let key_bytes: Vec<u8> = k.to_vec();
-                        let val_bytes: Vec<u8> = v.to_vec();
+                if let Ok(Some((k, v))) = original_queue.pop_min() {
+                    unacked_queue.insert(k.clone(), v.clone()).unwrap();
 
-                        let id = u64::from_be_bytes(key_bytes.try_into().unwrap());
+                    let key_bytes: Vec<u8> = k.to_vec();
+                    let val_bytes: Vec<u8> = v.to_vec();
 
-                        return Some(Message::new(id, Bytes::from(val_bytes)));
-                    }
+                    let id = u64::from_be_bytes(key_bytes.try_into().unwrap());
 
-                    None
-                });
+                    return Some(Message::new(id, Bytes::from(val_bytes)));
+                }
+
+                None
             })
             .await;
 
@@ -123,24 +104,20 @@ impl Storage {
 
     pub async fn ack_inner(&self, id: u64, queue: &str, application: &str) -> bool {
         let result = self
-            .execute_in_context(queue, move |db, queues| {
+            .execute_in_context(queue, move |db| {
                 let queue_names = NameUtils::application(application);
 
-                return queues.execute(db, queue_names.unacked(), |unacked_queue| {
-                    let id_vec = IVec::from(id.to_be_bytes().to_vec());
+                let unacked_queue = db.open_tree(queue_names.unacked()).unwrap();
 
-                    if let Ok(removed) = unacked_queue.remove(id_vec) {
-                        if removed.is_some() {
-                            info!(
-                            "removed form unacked queue, message_id={}, queue={}, application={}",
-                            id, queue, application
-                        );
-                            return true;
-                        }
+                let id_vec = IVec::from(id.to_be_bytes().to_vec());
+
+                if let Ok(removed) = unacked_queue.remove(id_vec) {
+                    if removed.is_some() {
+                        return true;
                     }
+                }
 
-                    false
-                });
+                false
             })
             .await;
 
@@ -149,43 +126,39 @@ impl Storage {
 
     pub async fn unack_inner(&self, id: u64, queue: &str, application: &str) -> bool {
         let result = self
-            .execute_in_context(queue, move |db, queues| {
+            .execute_in_context(queue, move |db| {
                 let queue_names = NameUtils::application(application);
 
-                return queues.execute(db, queue_names.unacked(), |unacked_queue| {
-                    let id_vec = IVec::from(id.to_be_bytes().to_vec());
+                let unacked_queue = db.open_tree(queue_names.unacked()).unwrap();
 
-                    //TODO REMOVE CLONE
-                    if let Ok(Some(removed_message)) = unacked_queue.remove(id_vec.clone()) {
-                        queues.execute(db, queue_names.default(), move |original_queue| {
-                            original_queue
-                                .insert(id_vec.clone(), removed_message.clone())
-                                .unwrap();
-                        });
+                let id_vec = IVec::from(id.to_be_bytes().to_vec());
 
-                        info!(
-                            "returned to original_queue, message_id={}, queue={}, application={}",
-                            id, queue, application
-                        );
-                    }
+                if let Ok(Some(removed_message)) = unacked_queue.remove(id_vec.clone()) {
+                    let original_queue = db.open_tree(queue_names.default()).unwrap();
 
-                    false
-                });
+                    original_queue
+                        .insert(id_vec.clone(), removed_message.clone())
+                        .unwrap();
+
+                    return true;
+                }
+
+                false
             })
             .await;
 
         result.unwrap_or(false)
     }
 
-    async fn execute_in_context<F: Fn(&Db, &QueueCollection) -> R, R>(
+    async fn execute_in_context<F: Fn(&Db) -> R, R>(
         &self,
         queue: &str,
         action: F,
     ) -> Result<R, String> {
         let read_lock = self.store.read().await;
 
-        if let Some((db, trees)) = read_lock.get(queue) {
-            let result = action(db, trees);
+        if let Some((db)) = read_lock.get(queue) {
+            let result = action(db);
 
             flush(db).await;
 
@@ -196,25 +169,20 @@ impl Storage {
             let mut write_lock = self.store.write().await;
 
             if let Ok(db) = sled::open(format!("{queue}.mesg")) {
-                let trees = QueueCollection::new();
-
-                action(&db, &trees);
+                let result = action(&db);
 
                 flush(&db).await;
 
-                write_lock.insert(String::from(queue), (db, trees));
+                write_lock.insert(String::from(queue), db);
+
+                return Ok(result);
             } else {
                 drop(write_lock);
 
                 let read_lock = self.store.read().await;
 
-                info!(
-                    "execute_in_context: another read_lock getted, queue={}",
-                    queue
-                );
-
-                if let Some((db, trees)) = read_lock.get(queue) {
-                    let result = action(db, trees);
+                if let Some((db)) = read_lock.get(queue) {
+                    let result = action(db);
 
                     flush(db).await;
 
@@ -234,11 +202,11 @@ impl Storage {
 
     pub async fn create_application_queue(&self, queue: &str, application: &str) -> bool {
         let result = self
-            .execute_in_context(queue, move |db, queues| {
+            .execute_in_context(queue, move |db| {
                 let queue_names = NameUtils::application(application);
 
-                queues.execute(db, queue_names.default(), move |_| {});
-                queues.execute(db, queue_names.unacked(), move |_| {});
+                db.open_tree(queue_names.default());
+                db.open_tree(queue_names.unacked());
 
                 info!(
                     "application queue created, queue={}, appplication={}",
@@ -250,6 +218,15 @@ impl Storage {
             .await;
 
         result.unwrap_or(false)
+    }
+
+    fn queue_names(db: &Db) -> Vec<String> {
+        db.tree_names()
+            .into_iter()
+            .filter(|n| n != b"__sled__default")
+            .map(|q| String::from_utf8(q.to_vec()).unwrap())
+            .filter(|q| !QueueNames::is_unacked(&q))
+            .collect()
     }
 }
 
