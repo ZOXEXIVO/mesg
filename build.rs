7f7410aa3eaadd92c64b86a9b11ccfa483c4f808
ZOXEXIVO <zoxexivo@gmail.com>
2022-08-25 01:47:25 +0300

    fix build

--- build.rs
+++ build.rs
@@ -1,11 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-         .build_server(true)
-         .build_client(false)
-         .out_dir("src/server/transport")
-         .compile(
-             &["src/server/transport/proto/mesg.proto"],
-             &["src"]
-         )?;
+        .build_server(true)
+        .build_client(false)
+        .out_dir("src/server/transport")
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["src/server/transport/proto/mesg.proto"], &["src"])?;
     Ok(())
- }
+}
--- testing/build.rs
+++ testing/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(true)
         .out_dir("src/")
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["../src/server/transport/proto/mesg.proto"], &[".."])?;
     Ok(())
 }
